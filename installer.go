b79ffb936ad8c3194c8b0a00770db5c8aa9af5c1
jakob <grandchild@gmx.net>
2016-07-19 00:15:16 +0200

    Quit on tab close, rewrite no-suffix to .html

--- installer.go
+++ installer.go
@@ -6,27 +6,31 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"os/signal"
+	"path/filepath"
+	"regexp"
+	"runtime"
 	"strings"
 	"time"
 
-	// "github.com/go-playground/statics/static"
 	"github.com/GeertJohan/go.rice"
+	"github.com/cloudfoundry/jibber_jabber"
 	"github.com/skratchdot/open-golang/open"
+	"golang.org/x/text/language"
 )
 
 var pBox *rice.Box
+var pushChan chan string
 
 func handler(response http.ResponseWriter, request *http.Request) {
-	isCommand, err := handleCommands(request.URL.Path, request.URL.Query())
-	if err != nil {
-		response.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(response, err)
-	}
+	isCommand := handleCommands(response, request.URL.Path, request.URL.Query())
 	if isCommand {
-		emptyOK(response)
 		return
 	}
-	str, err := getResource(pBox, request.URL.Path)
+	str, err := getResource(request.URL.Path)
+	if err != nil && !regexp.MustCompile(`\.[^/]+$`).MatchString(request.URL.Path) {
+		str, err = getResource(request.URL.Path + ".html")
+	}
 	if err != nil {
 		fmt.Println(err)
 		response.WriteHeader(http.StatusNotFound)
@@ -42,19 +46,25 @@ func emptyOK(response http.ResponseWriter) {
 	fmt.Fprint(response)
 }
 
-func handleCommands(path string, params url.Values) (bool, error) {
-	var err error
+func handleCommands(response http.ResponseWriter, path string, params url.Values) bool {
 	switch path {
 	case "/quit":
 		os.Exit(0)
-		return true, err
+	case "/os":
+		fmt.Fprint(response, runtime.GOOS)
+	case "/locale":
+		fmt.Fprint(response, getLocaleMatch())
 	case "/copy":
 		fmt.Println("src: " + params.Get("src") + " ---> dst: " + params.Get("dst"))
-		return true, err
+	case "/push":
+		// fmt.Println("Push channel opened.")
+		LaunchPush(response)
+	case "/strings":
+		fmt.Fprint(response, getAllLanguages())
 	default:
-		return false, err
+		return false
 	}
-
+	return true
 }
 
 func LaunchServer() int {
@@ -65,15 +75,93 @@ func LaunchInterface(port int) {
 	open.Run(fmt.Sprintf("http://localhost:%d/install", port))
 }
 
-func getResource(box *rice.Box, name string) (string, error) {
-	if box == nil {
-		return "", errors.New(fmt.Sprintf("payload '%s' doesn't exist.", box))
+// The SignalHandler sets up a channel to wait for an interrupt sent to the
+// program (Ctrl+C on the command line, most likely).
+// It then fires a goroutine which sends a "quit" command to the pushChan
+// channel, which in turn will send it to the interface, which will close or
+// deactivate the browser tab.
+func LaunchSignalHandler() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		pushChan <- "quit"
+		time.Sleep(500 * time.Millisecond)
+		os.Exit(0)
+	}()
+}
+
+// The Push request is a longstanding, continually refreshing request from the
+// interface which lets this server send commands back to the interface (while
+// normally the server can only answer to requests). Any string sent to the
+// pushChan go channel object will be sent to the interface as is and needs to
+// be handled there.
+func LaunchPush(response http.ResponseWriter) {
+	select {
+	case pushCommand := <-pushChan:
+		fmt.Fprint(response, pushCommand)
+	case <-time.After(1 * time.Minute):
+		fmt.Fprint(response, "refresh push")
 	}
-	text, err := box.String(name)
+}
+
+func getResource(name string) (string, error) {
+	return getResourceFiltered(name, regexp.MustCompile(`.*`))
+}
+func getResourceFiltered(name string, dirFilter *regexp.Regexp) (string, error) {
+	if pBox == nil {
+		return "", errors.New(fmt.Sprintf("payload '%s' doesn't exist.", pBox))
+	}
+	text, err := pBox.String(name)
 	if err != nil {
-		return "", errors.New(fmt.Sprintf("resource '%s' not found.", name))
+		contents := []string{}
+		err = pBox.Walk(name, func(path string, info os.FileInfo, err error) error {
+			if path != name {
+				if dirFilter.FindStringIndex(path) != nil {
+					contents = append(contents, path)
+				}
+				if info.IsDir() {
+					return filepath.SkipDir
+				}
+			}
+			return nil
+		})
+		if err == nil {
+			text = strings.Join(contents, "\n")
+		}
+	}
+	if err != nil {
+		return "", errors.New(fmt.Sprint(name, " not found."))
 	}
 	return text, err
+}
+
+func getLocaleMatch() string {
+	stringsFiles, _ := getResourceFiltered("strings", regexp.MustCompile(`\.json$`))
+	langCodes := strings.Split(regexp.MustCompile(`.*/([^/]+)\.json`).ReplaceAllString(stringsFiles, "$1"), "\n")
+	langTags := []language.Tag{language.Raw.Make("en")}
+	for _, lang := range langCodes {
+		if lang != "en" && lang != "" {
+			langTags = append(langTags, language.Raw.Make(lang))
+		}
+	}
+	locale, _ := jibber_jabber.DetectIETF()
+	match, _, _ := language.NewMatcher(langTags).Match(language.Make(locale))
+	return match.String()
+}
+
+func getAllLanguages() string {
+	stringsFiles, _ := getResourceFiltered("strings", regexp.MustCompile(`\.json$`))
+	langFiles := strings.Split(stringsFiles, "\n")
+	langCodes := strings.Split(regexp.MustCompile(`.*/([^/]+)\.json`).ReplaceAllString(stringsFiles, "$1"), "\n")
+	langs := []string{}
+	for i, lang := range langCodes {
+		if lang != "" {
+			langStrings, _ := getResource(langFiles[i])
+			langs = append(langs, "\""+lang+"\": "+langStrings)
+		}
+	}
+	return "{\n" + strings.Join(langs, ",\n") + "\n}"
 }
 
 func main() {
@@ -83,10 +171,12 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	pushChan = make(chan string, 1)
 	http.HandleFunc("/", handler)
 	port := LaunchServer()
-	fmt.Printf("Port: %d\n", port)
 	LaunchInterface(port)
+	LaunchSignalHandler()
+	fmt.Printf("Port: %d\n", port)
 	for {
 		time.Sleep(1 * time.Second)
 	}
--- payload/js/install.js
+++ payload/js/install.js
@@ -1,6 +1,65 @@
 
 
-window.onbeforeunload = function() {
-	alert("YO!!");
-	$.get('/quit');
+var os = '';
+var config = {};
+var locale = '';
+var strings = {};
+
+$(document).ready(function() {
+	pushHandler();
+	$.get('/os', function(data) { os = data.toLowerCase();
+	$.getJSON('/install.config', function(json) { config = json;
+	$.get('/locale', function(data) { locale = data;
+	$.getJSON('/strings', function(json) { strings = json;
+	loadInternationalStrings();
+	loadLanguage(locale);
+	})})})});
+});
+
+function loadInternationalStrings() {
+	for(var lang in strings) {
+		if(strings.hasOwnProperty(lang)) {
+			$('.ui-string.choose_language.'+lang).html(strings[lang]['choose_language']);
+		}
+	}
+}
+
+function loadLanguage(lang) {
+	for(var key in strings[lang]) {
+		if(strings[lang].hasOwnProperty(key)) {
+			var string = expandStringVariables(strings[lang][key], strings[lang]);
+			if (key==='title') {
+				document.title = string;
+			} else {
+				$('.ui-string.'+key).html(string);
+			}
+		}
+	}
+}
+
+function expandStringVariables(string, data) {
+	string = string.replace(/\$PRODUCT/, config['$PRODUCT']);
+	string = string.replace(/\$VERSION/, config['$VERSION']);
+	string = string.replace(/\$APPLAUNCHER/, data['_'+os+'_app_launcher']);
+	return string;
+}
+
+function pushHandler() {
+	$.get('/push', function(data) {
+		switch(data) {
+		case 'quit':
+			close();
+			alert('Installer was quit outside the browser. You should close this tab.')
+			break;
+		case 'refresh push':
+			pushHandler();
+			break;
+		default:
+			console.error('Unknown command from push channel: \''+data+'\'')
+		}
+	})
+}
+
+window.onbeforeunload = function(e) {
+	// $.get('/quit');
 };
