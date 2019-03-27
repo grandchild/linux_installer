6b51f7cc608adb049a84d529be15e301fec6e25b
jakob <grandchild@gmx.net>
2019-03-27 23:58:41 +0100

    Document gui.go

--- gui.go
+++ gui.go
@@ -15,18 +15,37 @@ import (
 )
 
 type (
-	EventHandler  map[string]interface{}
+	// EventHandler defines a function to be called when a certain event, identified by a
+	// string key, is emitted from the GTK GUI or manually.
+	EventHandler map[string]interface{}
+	// ScreenHandler is a name and a set of functions that corresponds to a specific
+	// screen in the installer process.
+	//
+	// before() is called immediately after the screen is presented, and allows
+	// initializing some GUI elements or running other setup code.
+	//
+	// after() is called immediately before the next (or previous) screen is presented, and
+	// is useful for applying the choices that were made in the still-current screen.
+	//
+	// undo() is called if the user navigated backwards. It is called immediately after
+	// after(). Undo must return a bool: if false is returned then screen switching is
+	// aborted. Useful for staying on the same screen, while the undo operation is in
+	// progress.
 	ScreenHandler struct {
 		name   string
 		before func()
 		after  func()
-		undo   func() bool // undo() should return true. if it returns false, then screen switching is cancelled.
+		undo   func() bool // if undo() returns false, then screen switching is canceled.
 	}
+	// Screen is a single step in the installer, such as the license screen, or selecting
+	// the installation path. It also contains its handler configuration.
 	Screen struct {
 		name    string
 		widget  *gtk.Box
 		handler ScreenHandler
 	}
+	// Gui defines a GTK3-based graphical interface for the installer in which a procession
+	// of screens must be followed, and includes the actual installation.
 	Gui struct {
 		installer        Installer
 		builder          *gtk.Builder
@@ -48,6 +67,8 @@ type (
 	}
 )
 
+// guiEventHandler returns an EventHandler that handles events from the GTK3 elements in
+// the .glade GUI-definition file, such as buttons, entries and window events.
 func guiEventHandler(g *Gui) (handler EventHandler) {
 	return EventHandler{
 		"on_quit_clicked":        func() { g.showQuitDialog() },
@@ -63,6 +84,9 @@ func guiEventHandler(g *Gui) (handler EventHandler) {
 	}
 }
 
+// internalEventHandler, as opposed to guiEventHandler, returns an EventHandler that
+// responds to events emitted by the behavioral GUI code, such as installation_finished,
+// or to update the progress bar during installation.
 func internalEventHandler(g *Gui) (handler EventHandler) {
 	return EventHandler{
 		"on_installation_finished": g.showResultScreen,
@@ -71,6 +95,9 @@ func internalEventHandler(g *Gui) (handler EventHandler) {
 	}
 }
 
+// screenHandlers registers a list of ScreenHandlers. See ScreenHandler for details.
+// This list defines the behavior of the individual screens, and is the central piece to
+// connect GUI interaction with the process of the installation.
 func screenHandlers(g *Gui) (handlers []ScreenHandler) {
 	return []ScreenHandler{
 		{
@@ -78,7 +105,7 @@ func screenHandlers(g *Gui) (handlers []ScreenHandler) {
 			before: func() {
 				g.backButton.SetSensitive(false)
 				g.setLabel("language-text", strings.Join(g.translator.GetAllVersionsList("_language_pick_text"), "\n"))
-				g.setLanguageOptions("language-choose", g.translator.GetLanguage())
+				g.setLanguageOptions("language-choose")
 			},
 			after: func() {
 				g.setLanguage("language-choose")
@@ -124,6 +151,10 @@ func screenHandlers(g *Gui) (handlers []ScreenHandler) {
 				g.quitButton.SetSensitive(false)
 				g.backButton.SetSensitive(false)
 				g.nextButton.SetLabel(g.t("button_exit"))
+				osCreateLauncherEntry(
+					g.translator.variables,
+					StringMap{"installDir": g.installer.Target},
+				)
 			},
 			after: func() {
 				gtk.MainQuit()
@@ -203,17 +234,25 @@ func NewGui(installerTempPath string, translator Translator, config *Config) (*G
 	return &gui, nil
 }
 
+// Run presents the GUI and starts the main event loop. When Run returns the application
+// is done and should quit.
 func (g *Gui) Run() {
 	g.win.ShowAll()
 	gtk.Main()
 }
 
+// showQuitDialog shows a dialog window to the user asking them whether they are sure
+// about exiting the installation before finishing. The decision is handled in an event
+// handler in guiEventHandler ("on_quit_no_clicked" & "on_quit_yes_clicked").
 func (g *Gui) showQuitDialog() {
 	g.translateAllLabels(getBox(g.builder, "quit-dialog-box"))
 	g.quitDialog.ShowAll()
 	g.quitDialog.GrabFocus()
 }
 
+// setScreenElementDefaults resets all navigation buttons to their defaults at the start
+// of each screen. Some of the buttons might have been altered by the screen handlers on
+// the previous screen.
 func (g *Gui) setScreenElementDefaults() {
 	g.backButton.SetSensitive(true)
 	g.nextButton.SetSensitive(true)
@@ -225,6 +264,9 @@ func (g *Gui) setScreenElementDefaults() {
 
 func (g *Gui) prevScreen() { g.showScreen(g.curScreen - 1) }
 func (g *Gui) nextScreen() { g.showScreen(g.curScreen + 1) }
+
+// showNamedScreen looks up and shows a specific screen by its name. If no screen by
+// that name is found, then nothing happens.
 func (g *Gui) showNamedScreen(name string) {
 	screenNum := int(-1)
 	for i, s := range g.screens {
@@ -237,6 +279,9 @@ func (g *Gui) showNamedScreen(name string) {
 		g.showScreen(screenNum)
 	}
 }
+
+// showScreen changes the screen and calls all available screen handler functions. The
+// num parameter is automatically clamped to the available screen indexes.
 func (g *Gui) showScreen(num int) {
 	if num >= 0 && num < len(g.screens) {
 		g.screenChangeLock.Lock()
@@ -261,6 +306,8 @@ func (g *Gui) showScreen(num int) {
 	}
 }
 
+// browseInstallDir opens a GTK file chooser and fills the path edit field with the
+// results.
 func (g *Gui) browseInstallDir() {
 	chooser, err := gtk.FileChooserDialogNewWith2Buttons(
 		g.t("dir_browse_title"), g.win,
@@ -279,6 +326,9 @@ func (g *Gui) browseInstallDir() {
 	chooser.Close()
 }
 
+// resetInstallDir resets the path edit field to the predefined default path, which is a
+// subdirectory within the user's home. The string for the subdirectory may be set in
+// the config through "default_install_dir_name".
 func (g *Gui) resetInstallDir() {
 	g.dirPathEdit.SetText(filepath.Join(
 		glib.GetHomeDir(),
@@ -286,6 +336,10 @@ func (g *Gui) resetInstallDir() {
 	))
 }
 
+// checkInstallDir is run whenever the path edit content changes, and tests whether the
+// path is valid, its parent writable and whether there is enough space on the disk that
+// the path is on. In case of errors it prints them to the (usually invisible) error
+// label in the GUI.
 func (g *Gui) checkInstallDir() {
 	g.nextButton.SetSensitive(true)
 	dirName, _ := g.dirPathEdit.GetText()
@@ -307,12 +361,12 @@ func (g *Gui) checkInstallDir() {
 
 // t returns a localized string for the key, and expands any template variables therein.
 // Variables are surrounded by double braces and preceded by a dot like this:
-//
 // 	{{.var}}
 func (g *Gui) t(key string) (localized string) {
 	return g.translator.Get(key)
 }
 
+// setLabel changes the text on a label with the given labelId to the given string.
 func (g *Gui) setLabel(labelId string, content string) error {
 	label := getLabel(g.builder, labelId)
 	if label == nil {
@@ -322,13 +376,16 @@ func (g *Gui) setLabel(labelId string, content string) error {
 	return nil
 }
 
-func (g *Gui) setLanguageOptions(chooserId string, defaultLang string) error {
+// setLanguageOptions fills the dropdown selection box with any language that is
+// available through the GUI's translator object and selects the default language.
+func (g *Gui) setLanguageOptions(chooserId string) error {
 	comboBox := getComboBoxText(g.builder, chooserId)
 	if comboBox == nil {
 		return errors.New(fmt.Sprintf("No Dropdown '%s'", chooserId))
 	}
 	comboBox.RemoveAll()
 	langList := g.translator.GetLanguages()
+	defaultLang := g.translator.GetLanguage()
 	displayStrings := g.translator.GetAllVersions("_language_display")
 	for _, id := range langList {
 		comboBox.Append(id, displayStrings[id])
@@ -339,6 +396,8 @@ func (g *Gui) setLanguageOptions(chooserId string, defaultLang string) error {
 	return nil
 }
 
+// setLanguage gets the currently selected language in the dropdown selection, and
+// applies it to all texts throughout the GUI, on all screens.
 func (g *Gui) setLanguage(chooserId string) error {
 	comboBox := getComboBoxText(g.builder, chooserId)
 	if comboBox == nil {
@@ -364,6 +423,8 @@ func (g *Gui) setLanguage(chooserId string) error {
 	return nil
 }
 
+// translateAllLabels searches recursively, starting with the given GTK item, for labels
+// or buttons to translate and translates their contents.
 func (g *Gui) translateAllLabels(item interface{}) {
 	switch widget := item.(type) {
 	case *gtk.Box:
@@ -385,6 +446,8 @@ func (g *Gui) translateAllLabels(item interface{}) {
 	}
 }
 
+// translateLabel searches for a variable (surrounded by "$" like "$variable$") in the
+// label's text and replaces it with the variable's string value from the translator.
 func (g *Gui) translateLabel(label *gtk.Label) {
 	variable := regexp.MustCompile(`\$[a-zA-Z0-9_]+\$`).FindString(label.GetLabel())
 	if len(variable) > 2 {
@@ -392,6 +455,8 @@ func (g *Gui) translateLabel(label *gtk.Label) {
 	}
 }
 
+// translateButton searches for a variable (surrounded by "$" like "$variable$") in the
+// button's text and replaces it with the variable's string value from the translator.
 func (g *Gui) translateButton(button *gtk.Button) {
 	buttonLabel, err := button.GetLabel()
 	if err != nil {
@@ -403,6 +468,9 @@ func (g *Gui) translateButton(button *gtk.Button) {
 	}
 }
 
+// installationProgress is a glib.Idle function that gets called repeatedly unless it
+// returns false. During the file copy process this function checks on the status of the
+// installer and emits update signals for the progress bar and finish or undo handlers.
 func (g *Gui) installationProgress() (repeat bool) {
 	status := g.installer.Status()
 	g.win.Emit("update_progressbar")
@@ -417,6 +485,8 @@ func (g *Gui) installationProgress() (repeat bool) {
 	return true
 }
 
+// updateProgressbar updates the progress bar with the current filename that the
+// percentage of bytes copies to disk so far.
 func (g *Gui) updateProgressbar() {
 	installingFile := g.installer.NextFile()
 	if installingFile != nil {
@@ -425,6 +495,9 @@ func (g *Gui) updateProgressbar() {
 	g.progressBar.SetProgressFraction(g.installer.Progress())
 }
 
+// showResultScreen gets called after the file copy process stops, checks on the status
+// of the installer, and changes to the appropriate final screen of the installer GUI,
+// success or failure.
 func (g *Gui) showResultScreen() {
 	if g.installer.err != nil {
 		log.Println(g.installer.err.Error())
