53d31550f52ca83dac8601333a514884d606f6fd
jakob <grandchild@gmx.net>
2019-03-27 22:51:45 +0100

    Add Linux launcher shortcut creator

--- install_unix.go
+++ install_unix.go
@@ -2,7 +2,29 @@
 
 package linux_installer
 
-import "golang.org/x/sys/unix"
+import (
+	"io/ioutil"
+	"os/user"
+	"path/filepath"
+
+	"golang.org/x/sys/unix"
+)
+
+const (
+	desktopFileUserDir   = ".local/share/applications"
+	desktopFileSystemDir = "/usr/share/applications"
+	desktopFilename      = "acme-exampleapp.desktop"
+	desktopFileTemplate  = `[Desktop Entry]
+Name={{.product}}
+Version={{.version}}
+Type=Application
+Icon={{.installDir}}/ExampleApp.png
+Exec={{.installDir}}/ExampleApp
+Comment={{.tagline}}
+Categories=Simulation;Engineering;Science;
+Terminal=true
+`
+)
 
 func osFileWriteAccess(path string) bool {
 	return unix.Access(path, unix.W_OK) == nil
@@ -14,4 +36,20 @@ func osDiskSpace(path string) int64 {
 		return -1
 	}
 	return int64(fs.Bavail) * fs.Bsize
+}
+
+func osCreateLauncherEntry(variables ...StringMap) error {
+	content := ExpandVariables(desktopFileTemplate, MergeVariables(variables...))
+	usr, err := user.Current()
+	if err != nil {
+		return err
+	}
+	var applicationsDir string
+	if usr.Uid == 0 {
+		applicationsDir = desktopFileSystemDir
+	} else {
+		applicationsDir = desktopFileUserDir
+	}
+	desktopFilepath := filepath.Join(usr.HomeDir, applicationsDir, desktopFilename)
+	return ioutil.WriteFile(desktopFilepath, []byte(content), 0755)
 }
