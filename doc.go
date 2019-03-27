a23ffb54d0b63ec23565bf9b6900e88e620081fd
jakob <grandchild@gmx.net>
2019-03-27 11:18:38 +0100

    Add package documentation

---
+++ doc.go
@@ -0,0 +1,8 @@
+// A simple installer builder for Linux systems.
+//
+// This installer will bundle any files and folders inside the "data" directory, to be
+// installed via a GTK3-based GUI, or alternatively a commandline interface (referred to
+// as a "silent" install).
+//
+// TODO(jakob): write detailed description of code layout.
+package linux_installer
